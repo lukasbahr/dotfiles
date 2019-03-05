b505f9ed4fbfc2e81b220f9c4863a97190e49455
lukasbahr <lukas.bahr@rwth-aachen.de>
2019-03-05 20:57:57 +0100

    install.sh: updated

--- install.sh
+++ install.sh
@@ -4,15 +4,13 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
+dir=~/dotfiles			  # dotfiles directory
+olddir=~/dotfiles_old		  # old dotfiles backup directory
+files="	.config/nvim
+	.zshrc"
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="
-			.config/nvim
-			.zshrc 
-			"
-##########
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"

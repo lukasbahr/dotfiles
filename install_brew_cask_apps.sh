f09a00eedea1d6b60be3c017deb4b8149e187f51
lukasbahr <lukas.bahr@rwth-aachen.de>
2019-10-10 15:43:03 +0200

    added brew formular

---
+++ install_brew_cask_apps.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo Install Mac App Store apps first.
+
+# Either use mas-cli (https://github.com/argon/mas) or install manually; apps I need: Bear/Simplenote, Tyme, Polarr, Pixelmator, JPEGmini.
+read -p "Press any key to continueÉ " -n1 -s
+echo '\n'
+
+# Check that Homebrew is installed and install if not
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+fi
+
+# Update any existing homebrew recipes
+brew update
+
+# Upgrade any already installed formulae
+brew upgrade --all
+
+# Install my brew packages
+brew install wget
+
+# Install cask
+brew tap phinze/homebrew-cask
+
+# Install desired cask packages
+brew cask install alfred
+brew cask install the-unarchiver
+brew cask install miniconda
+brew cask install anki
+brew cask install calibre
+brew cask install lastpass
+brew cask install notion
+brew cask install sync-my-l2p
+brew cask install spotify
+
+
+# Remove brew cruft
+brew cleanup
+
+# Remove cask cruft
+brew cask cleanup
+
+# Link alfred to apps
+brew cask alfred link
--- install_macos.sh
+++ install_macos.sh
@@ -7,6 +7,7 @@
 dir=~/dotfiles			  # dotfiles directory
 olddir=~/dotfiles_old		  # old dotfiles backup directory
 files="	.config/nvim
+	.config/tmux
 	.config/kitty
 	.zshrc"
 
